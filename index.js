c38835439e2bbd614c8c1fbb3080abc503f43305
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-15 16:27:33 -0700

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+var header = $('h1');
+var coding = $('.coding')
+var codeWorkAhead = $('#codeWorkAhead')
+// console.log(header);
+// console.log(coding);
+// console.log(codeWorkAhead);
+coding.text("We're GUCCI in the FLIP FLOPS!");
+
+var button = $('button');
+button.on("click", doSomething);
+function doSomething(){
+  console.log("Thanks for clicking me, you're a real pal! -Your BFF, Button :)")
+}
